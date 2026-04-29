7baefc66d543b8c155e27fbe0cc2779a4ea71b6e
GerrittHooyer <gerritt.hooyer@gmail.com>
2026-04-28 20:32:02 -0400

    Perlin Noise MapAlgorithm implemented.

--- Runtime/Algorithms/MultiMapAlgorithm.cs
+++ Runtime/Algorithms/MultiMapAlgorithm.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using RangeAttribute = UnityEngine.RangeAttribute;
 
 [CreateAssetMenu(fileName = "MultiMapAlgorithm", menuName = "Scriptable Objects/MapAlgorithm/MultiMapAlgorithm")]
 public class MultiMapAlgorithm : MapAlgorithm
 {
     [SerializeField] List<MapAlgorithm> _algorithms;
-
     public override void Generate()
     {
         foreach (var algorithm in _algorithms)
